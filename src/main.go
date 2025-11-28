94916871131d0fdd1fc4d7f874020471e5ee0d2f
Mike Taylor <mike@indexdata.com>
2025-11-28 12:16:08 +0000

    First running main function

--- src/main.go
+++ src/main.go
@@ -2,6 +2,24 @@ package main
 
 import "os"
 import "fmt"
+import "regexp"
+import "github.com/MikeTaylor/catlogger"
+
+func makeConfiguredLogger() *catlogger.Logger {
+	// catlogger.MakeLogger handes the category environment variables on its own
+	prefix := os.Getenv("LOGGING_PREFIX")
+	timestamp := false
+	tsString := os.Getenv("LOGGING_TIMESTAMP")
+	if tsString != "" {
+		timestamp = true
+	}
+
+	logger := catlogger.MakeLogger("", prefix, timestamp)
+
+	// We do not need this transformation yet, but will need something like it when we have authentication
+	logger.AddTransformation(regexp.MustCompile(`\\"pass\\":\\"[^"]*\\"`), `\"pass\":\"********\"`)
+	return logger
+}
 
 func main() {
 	if len(os.Args) != 1 {
@@ -9,19 +27,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	logger := makeConfiguredLogger()
+	logger.Log("hello", "Hello, world!")
+
 	/*
-	server, err := MakeCyclopsServer(".")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: cannot create server: %s\n", os.Args[0], err)
-		os.Exit(2)
-	}
+		server, err := MakeModCyclopsServer(logger, ".")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: cannot create server: %s\n", os.Args[0], err)
+			os.Exit(2)
+		}
 
-	err = server.launch()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: cannot launch server: %s\n", os.Args[0], err)
-		os.Exit(3)
-	}
+		err = server.launch()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: cannot launch server: %s\n", os.Args[0], err)
+			os.Exit(3)
+		}
 	*/
-
-	fmt.Printf("Hello, world!\n")
 }
